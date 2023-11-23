6791b73be53059885b4004f11a835e6704b7ed77
g0mteng <gomteng03@naver.com>
2023-11-24 04:44:27 +0900

    post

--- lib/view/team_add_page.dart
+++ lib/view/team_add_page.dart
@@ -14,8 +14,8 @@ class _TeamSpaceAddPageState extends State<TeamSpaceAddPage> {
   final TextEditingController capacityController = TextEditingController();
 
   void handleSubmit() async {
-    final String name = nameController.text ?? '';
-    final String profileImgUrl = profileImgUrlController.text ?? '';
+    final String name = nameController.text;
+    final String profileImgUrl = profileImgUrlController.text;
     final int capacity = int.tryParse(capacityController.text) ?? 0;
 
     TeamSpaceAddModel teamSpaceAddModel = TeamSpaceAddModel(
@@ -24,34 +24,44 @@ class _TeamSpaceAddPageState extends State<TeamSpaceAddPage> {
       capacity: capacity,
     );
 
-    // 서버로 데이터 전송
-    await sendTeamSpaceData(teamSpaceAddModel);
+    // 서버로 데이터 전송하는 로직 구현
+    // 예: await sendTeamSpaceData(teamSpaceAddModel);
+
+    // 서버 응답에 따른 UI 반응 구현
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CreateTeamAppBar(),
-      body: Column(
-        children: <Widget>[
-          TextField(
-            controller: nameController,
-            decoration: const InputDecoration(labelText: 'Name'),
-          ),
-          TextField(
-            controller: profileImgUrlController,
-            decoration: const InputDecoration(labelText: 'Profile Image URL'),
-          ),
-          TextField(
-            controller: capacityController,
-            decoration: const InputDecoration(labelText: 'Capacity'),
-            keyboardType: TextInputType.number,
-          ),
-          ElevatedButton(
-            onPressed: handleSubmit,
-            child: const Text('Submit'),
-          ),
-        ],
+      appBar: const CreateTeamAppBar(), // AppBar 스타일 유지
+      body: Padding(
+        padding: const EdgeInsets.all(20.0), // Padding 추가
+        child: Column(
+          children: <Widget>[
+            // TextField 위젯들 구성
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              controller: profileImgUrlController,
+              decoration: const InputDecoration(labelText: 'Profile Image URL'),
+            ),
+            TextField(
+              controller: capacityController,
+              decoration: const InputDecoration(labelText: 'Capacity'),
+              keyboardType: TextInputType.number,
+            ),
+            const SizedBox(height: 20), // 간격 추가
+            ElevatedButton(
+              onPressed: handleSubmit,
+              child: const Text('Submit'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(pColor), // 버튼 색상 조정
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -70,5 +80,5 @@ class CreateTeamAppBar extends StatelessWidget implements PreferredSizeWidget {
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
